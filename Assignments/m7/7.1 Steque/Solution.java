0d99dc32708f178e202740bbff39b26f12af54fd
heerak205 <heerak.praffull@msitprogram.net>
2018-10-01 18:10:30 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ Assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+class Node{
+	String data;
+	Node next;
+	Node(String item){
+		this.data = item;
+	}
+}
+class Steque{
+	Node start, end;
+	int size = 0;
+	Steque(){
+		start = null;
+		end = null;
+	}
+	void push(String item){
+		Node obj = new Node(item);
+		if(size == 0){
+			start = obj;
+			end = start;
+			size++;
+			return;
+		}
+		obj.next = start;
+		start = obj;
+		size++;
+
+	}
+	void enqueue(String item){
+		Node obj = new Node(item);
+		if(size == 0){
+			start = obj;
+			end = start;
+			size++;
+			return;
+		}
+		end.next = obj;
+		end = obj;
+		size++;
+	}
+	String pop() throws Exception{
+		if(isEmpty()){
+			throw new Exception("Steque is empty");
+		}
+		String data = start.data;
+		start = start.next;
+		size--;
+		return data;
+	}
+	boolean isEmpty(){
+		return size == 0;
+	}
+	public String toString(){
+		if(isEmpty()){
+
+		}
+		Node temp = start;
+		String res = "";
+		while(temp!=null) {
+			res += temp.data + ", ";
+			temp = temp.next;
+		}
+		return res.substring(0,res.length()-2);
+	}
+}
+class Solution{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = Integer.parseInt(sc.nextLine());
+		Steque st = new Steque();
+		while(sc.hasNextLine()){
+			String input[] = sc.nextLine().split(" ");
+			switch(input[0]) {
+				case "push":
+				st.push(input[1]);
+				System.out.println(st);
+				break;
+				case "pop":
+				try{
+					st.pop();
+					if(st.isEmpty()){
+						System.out.println("Steque is empty.");
+					} else{
+						System.out.println(st);
+					}
+				}
+				catch (Exception e){
+					System.out.println(e.getMessage());
+				}
+				break;
+				case "enqueue":
+				st.enqueue(input[1]);
+				System.out.println(st);
+				break;
+				default:
+					st = new Steque();
+					System.out.println();
+					break;
+			}
+		}
+	}
+}
