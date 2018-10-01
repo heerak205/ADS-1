84f782ee6c27bccc127470e7b93e4d38c836f7cc
heerak205 <heerak.praffull@msitprogram.net>
2018-10-01 18:21:52 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- Assignments/m7/7.1 Steque/Solution.java
+++ Assignments/m7/7.1 Steque/Solution.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-class Node{
+class Node {
 	String data;
 	Node next;
-	Node(String item){
+	Node(String item) {
 		this.data = item;
 	}
 }
-class Steque{
+class Steque {
 	Node start, end;
 	int size = 0;
-	Steque(){
+	Steque() {
 		start = null;
 		end = null;
 	}
-	void push(String item){
+	void push(String item) {
 		Node obj = new Node(item);
-		if(size == 0){
+		if (size == 0) {
 			start = obj;
 			end = start;
 			size++;
@@ -26,9 +26,9 @@ class Steque{
 		size++;
 
 	}
-	void enqueue(String item){
+	void enqueue(String item) {
 		Node obj = new Node(item);
-		if(size == 0){
+		if (size == 0) {
 			start = obj;
 			end = start;
 			size++;
@@ -38,8 +38,8 @@ class Steque{
 		end = obj;
 		size++;
 	}
-	String pop() throws Exception{
-		if(isEmpty()){
+	String pop() throws Exception {
+		if (isEmpty()) {
 			throw new Exception("Steque is empty.");
 		}
 		String data = start.data;
@@ -47,55 +47,54 @@ class Steque{
 		size--;
 		return data;
 	}
-	boolean isEmpty(){
+	boolean isEmpty() {
 		return size == 0;
 	}
-	public String toString(){
-		if(isEmpty()){
+	public String toString() {
+		// if(isEmpty()){
 
-		}
+		// }
 		Node temp = start;
 		String res = "";
-		while(temp!=null) {
+		while (temp != null) {
 			res += temp.data + ", ";
 			temp = temp.next;
 		}
-		return res.substring(0,res.length()-2);
+		return res.substring(0, res.length() - 2);
 	}
 }
-class Solution{
+class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = Integer.parseInt(sc.nextLine());
 		Steque st = new Steque();
-		while(sc.hasNextLine()){
+		while (sc.hasNextLine()) {
 			String input[] = sc.nextLine().split(" ");
-			switch(input[0]) {
-				case "push":
+			switch (input[0]) {
+			case "push":
 				st.push(input[1]);
 				System.out.println(st);
 				break;
-				case "pop":
-				try{
+			case "pop":
+				try {
 					st.pop();
-					if(st.isEmpty()){
+					if (st.isEmpty()) {
 						System.out.println("Steque is empty.");
-					} else{
+					} else {
 						System.out.println(st);
 					}
-				}
-				catch (Exception e){
+				} catch (Exception e) {
 					System.out.println(e.getMessage());
 				}
 				break;
-				case "enqueue":
+			case "enqueue":
 				st.enqueue(input[1]);
 				System.out.println(st);
 				break;
-				default:
-					st = new Steque();
-					System.out.println();
-					break;
+			default:
+				st = new Steque();
+				System.out.println();
+				break;
 			}
 		}
 	}
